8422fbb33dc2b04db2ad3f16e75f5336ff8305eb
Habibur Rahman <hrabid@outlook.com>
2025-05-15 08:39:31 +0600

    Initial Commit

---
+++ alias.sh
@@ -0,0 +1,19 @@
+# System Navigation Aliases
+## Aliases Related to listing Directories
+alias ls=lsd
+alias lt='ls --tree'
+alias lsa='ls -a'
+alias llt='ls -l --tree'
+alias lta='ls -a --tree'
+
+## Aliases Related to Changing Directories
+alias cdp='cd ..'
+
+## Aliases Related to fzf
+
+## Terminal
+alias c=clear
+alias e=exit
+
+## Editor Aliases
+alias v=nvim
---
+++ scripts/rfonts.sh
@@ -0,0 +1,20 @@
+sudo dnf remove \
+google-noto-sans-cjk-ttc-fonts \
+google-noto-sans-hebrew-vf-fonts \
+# google-noto-sans-arabic-vf-fonts \
+google-noto-sans-devanagari-vf-fonts \
+# google-noto-sans-bengali-vf-fonts \
+google-noto-sans-thai-vf-fonts \
+google-noto-sans-tamil-vf-fonts \
+google-noto-sans-ethiopic-vf-fonts \
+google-noto-sans-mono-vf-fonts \
+google-noto-serif-*-fonts \
+lohit-*-fonts \
+paktype-*-fonts \
+aajohan-comfortaa-fonts \
+sil-abyssinica-fonts \
+sil-nuosu-fonts \
+smc-meera-fonts \
+cjkuni-ukai-fonts \
+cjkuni-uming-fonts
+
